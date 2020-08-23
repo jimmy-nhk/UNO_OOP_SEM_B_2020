f1d94ece7b30f8f260bf892f51116cff7ae38495
Jimmy <s3802040@rmit.edu.vn>
2020-08-23 10:30:22 +0700

    Main menu of Jimmy nha

---
+++ src/Jimmy/MainMenu.java
@@ -0,0 +1,22 @@
+package Jimmy;
+
+import javafx.event.ActionEvent;
+
+public class MainMenu {
+
+
+    public void playGame(ActionEvent actionEvent) {
+    }
+
+    public void showLeaderBoard(ActionEvent actionEvent) {
+    }
+
+    public void showInstruction(ActionEvent actionEvent) {
+    }
+
+    public void showSettings(ActionEvent actionEvent) {
+    }
+
+    public void quit(ActionEvent actionEvent) {
+    }
+}
