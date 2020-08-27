7ab2450d97a1e92ce7d1425a7ea68968b8222cc5
s3819660 <s3819660@rmit.edu.vn>
2020-08-27 23:46:31 +0700

    declare class PlayerList and method load, save, displayLeaderboard and sortRanking

--- src/Model/PlayerList.java
+++ src/Model/PlayerList.java
@@ -1,4 +1,30 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class PlayerList {
+    private ArrayList<ACPlayer> playerList = new ArrayList<>();
+
+    public ArrayList<ACPlayer> getPlayerList() {
+        return playerList;
+    }
+
+    public void setPlayerList(ArrayList<ACPlayer> playerList) {
+        this.playerList = playerList;
+    }
+
+    public static void loadPlayer() {
+
+    }
+
+    public static void savePlayer() {
+
+    }
+
+    public static void displayLeaderboard() {
+    }
+
+    public static void sortRanking() {
+
+    }
 }
