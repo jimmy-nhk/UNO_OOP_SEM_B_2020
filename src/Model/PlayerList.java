24707ce9454a0ee5501bacecd347064263a1af76
s3819660 <s3819660@rmit.edu.vn>
2020-08-28 16:37:26 +0700

    redeclare class PlayerList and method load, save, displayLeaderboard and sortRanking

--- src/Model/PlayerList.java
+++ src/Model/PlayerList.java
@@ -1,4 +1,56 @@
 package Model;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class PlayerList {
+    ArrayList<Account> playerList = new ArrayList<>();
+
+    public void loadFromFile() {
+        try
+        {
+            FileInputStream fis = new FileInputStream("employeeData");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            playerList = (ArrayList) ois.readObject();
+
+            ois.close();
+            fis.close();
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+            return;
+        }
+        catch (ClassNotFoundException c)
+        {
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return;
+        }
+    }
+
+    public void saveToFile() {
+        try
+        {
+            FileOutputStream fos = new FileOutputStream("employeeData");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(playerList);
+            oos.close();
+            fos.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayLeaderboard() {
+
+    }
+
+    public void sortRanking() {
+        Collections.sort(playerList);
+    }
 }
