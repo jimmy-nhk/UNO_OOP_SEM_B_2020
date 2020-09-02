b94d8c6154c3d5cce73a8646f384a11f9b306315
s3819660 <s3819660@rmit.edu.vn>
2020-09-03 00:24:09 +0700

    accountList changed to static attribute because it needs a lot of modifying, checkAccountExist + signIn() => getAccountIndex, must copy back to accountList when winning, losing because Account attribute in Player is just a copy of an Account from AccountList.accountList

--- src/Model/AccountList.java
+++ src/Model/AccountList.java
@@ -1,4 +1,94 @@
 package Model;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class AccountList {
+    static ArrayList<Account> accountList = new ArrayList<>();
+
+    public void loadFromFile() {
+        /**
+         * Deserialize data from file
+         */
+        try
+        {
+            FileInputStream fis = new FileInputStream("data.uno");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            accountList = (ArrayList) ois.readObject();
+
+            ois.close();
+            fis.close();
+        }
+        catch (IOException | ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Serialize data from file
+     */
+    public void saveToFile() {
+        try
+        {
+            FileOutputStream fos = new FileOutputStream("data.uno");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(accountList);
+            oos.close();
+            fos.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Display players on leaderboard
+     */
+    public String displayLeaderboard() {
+        return accountList.toString();
+    }
+
+    /**
+     * Sorting players by points
+     */
+    public void setRanking() {
+        Collections.sort(accountList);
+    }
+
+    /**
+     * Check if account already exists by searching for matching name and password, return -1 if account does not exist
+     */
+    public int getAccountIndex(String name, String password) {
+        for (Account a: accountList) {
+            if (name.equals(a.getName()) && password.equals(a.getPassword()))
+                return accountList.indexOf(a);
+        }
+        return -1;
+    }
+
+    /**
+     * return a player when account exists, null when account does not exist
+     */
+    public Player signIn(String name, String password) {
+        if (getAccountIndex(name, password) >= 0) {
+            return new Player(accountList.get(getAccountIndex(name, password)), null);
+        }
+        return null;
+    }
+
+    /**
+     * Sign up account, return true if sign up successfully, false if account already exists
+     */
+    public boolean signUp(String name, String password) {
+        if (getAccountIndex(name, password) >= 0) {
+            return false;
+        }
+        Account account = new Account(name, password, 0, 0);
+        accountList.add(account);
+        return true;
+    }
 }
