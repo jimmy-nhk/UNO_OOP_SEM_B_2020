944b7a949c6129c3e632e6920010659a6076b018
ChauHuynh1 <s3777214@rmit.edu.vn>
2020-09-15 08:55:26 +0700

    Sound game

--- src/Controller/SettingController.java
+++ src/Controller/SettingController.java
@@ -1,10 +1,20 @@
 package Controller;
 
+import Model.Sound;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
-public class SettingController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SettingController implements Initializable {
+    public Button Volume;
+    public Button Language;
+    public Button Theme;
+    public Button backButton;
     @FXML
     private VBox settingBoard;
     private MainMenu mainMenu;
@@ -16,5 +26,26 @@ public class SettingController {
     public void backMainMenu(MouseEvent mouseEvent) {
         settingBoard.setVisible(false);
         mainMenu.getMainMenu().setVisible(true);
+    }
+
+
+
+    public void setSoundBtnEventHandler() {
+        this.Volume.setOnMouseClicked((MouseEvent e) -> {
+            Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+        });
+        this.Language.setOnMouseClicked((MouseEvent e) -> {
+            Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+        });
+        this.Theme.setOnMouseClicked((MouseEvent e) -> {
+            Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+        });
+        this.backButton.setOnMouseClicked((MouseEvent e) -> {
+            Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+        });
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setSoundBtnEventHandler();
     }
 }
