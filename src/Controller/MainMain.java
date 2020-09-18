d74f0739e7ced969170c561f53c713f727477729
Huynh Chau <48887152+ChauHuynh1@users.noreply.github.com>
2020-09-18 16:06:18 +0700

    Update MainMain.java

--- src/Controller/MainMain.java
+++ src/Controller/MainMain.java
@@ -1,10 +1,7 @@
 package Controller;
 
 import Model.AccountList;
-import Model.Message;
 import Model.Sound;
-import Model.Timer;
-import Service.GameBoardService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,41 +9,70 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MainMain implements Initializable {
 
-    @FXML public Pane logInScene;
-    @FXML public VBox mainMenu;
-    @FXML public VBox instruction;
+    @FXML
+    public Pane logInScene;
+    @FXML
+    public VBox mainMenu;
+    @FXML
+    public VBox instruction;
 
-    @FXML public VBox leaderboard;
-    @FXML public VBox settingBoard;
-    @FXML private Pane chooseModeScene;
-    @FXML private Pane setNameScene;
-    @FXML private AnchorPane readyScene;
+    @FXML
+    public VBox leaderboard;
+    @FXML
+    public VBox settingBoard;
+    @FXML
+    public Button volume;
+    @FXML
+    public Button language;
+    @FXML
+    public Button backButton3;
+    @FXML
+    public Button theme;
+    @FXML
+    public Button quit;
+    public Button goOnlineScene;
+    public Button backButton2;
+    public Button goOnline;
+    public Button goOffline;
+    public Button startButton;
+    public Button leaderBoard;
+    public Button instructions;
+    public Button settings;
+    public Label welcome;
+    public Label leaderBoardLabel;
+    public Button backButton1;
+    public Label instructionLabel;
+    public Label settingLabel;
+    @FXML
+    private Pane chooseModeScene;
+    @FXML
+    private Pane setNameScene;
+    @FXML
+    public static Sound backGroundSound = new Sound("src/resources/sound/background.mp3");
+
     @FXML
     private TextField txtName;
     @FXML
     private PasswordField passwordField;
 
-    private int total;
+
     private String name;
     private String password;
     private AccountList accountList;
-    private Message message = new Message("start");
+    public static Locale locale;
 
     public void displaySignUpMessage(ActionEvent actionEvent) {
         Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
@@ -72,8 +98,9 @@ public class MainMain implements Initializable {
 
     }
 
-    public void displaySignInMessage(ActionEvent actionEvent) throws IOException {
+    public void displaySignInMessage(ActionEvent actionEvent) {
         Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+//        backGroundSound = new Sound("src/resources/sound/background.mp3");
         name = txtName.getText();
         password = passwordField.getText();
 
@@ -90,8 +117,7 @@ public class MainMain implements Initializable {
             signUpBox.setResult(ButtonType.OK);
             signUpBox.showAndWait();
             Sound buttonSound1 = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
-            Message message = new Message("login",name,password);
-            GameBoard.clientController.writeMessage(message);
+
             logInScene.setVisible(false);
             mainMenu.setVisible(true);
         }
@@ -109,18 +135,14 @@ public class MainMain implements Initializable {
     public void playGame(ActionEvent actionEvent) throws IOException {
         /** Will go to the gameBoard or the selection scene **/
         Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
-        total ++;
-        mainMenu.setVisible(false);
-        readyScene.setVisible(true);
-        if (getTotal()==4) {
-//            start scene
-        }
-//        Parent view2 = FXMLLoader.load(getClass().getClassLoader().getResource("resources/view/GameBoard.fxml"));
-//        Scene scene = new Scene(view2);
-//
-//        Stage newWindow = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-//        newWindow.setScene(scene);
-//        newWindow.show();
+
+        Parent view2 = FXMLLoader.load(getClass().getClassLoader().getResource("resources/view/GameBoard.fxml"));
+        Scene scene = new Scene(view2);
+
+        Stage newWindow = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        newWindow.setScene(scene);
+//        setLanguageForGameBoard();
+        newWindow.show();
 
     }
 
@@ -137,7 +159,6 @@ public class MainMain implements Initializable {
         mainMenu.setVisible(false);
         instruction.setVisible(true);
 
-
     }
 
     public void showSettings(ActionEvent actionEvent) {
@@ -145,11 +166,79 @@ public class MainMain implements Initializable {
 
         mainMenu.setVisible(false);
         settingBoard.setVisible(true);
+
+        setButtonActionForVolume();
+        setButtonActionForLanguage();
+        setButtonActionForTheme();
+        this.backButton3.setOnMouseClicked(this::backMainMenuFromSetting);
+    }
+
+    private void setButtonActionForVolume() {
+        this.volume.setOnMouseClicked((MouseEvent e) -> {
+            Sound buttonSound1 = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+            backGroundSound.stop();
+            this.volume.setOnMouseClicked((MouseEvent e1) -> {
+                Sound buttonSound2 = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+                backGroundSound.play();
+                setButtonActionForVolume();
+            });
+        });
+    }
+
+    private void setButtonActionForLanguage() {
+        this.language.setOnMouseClicked((MouseEvent e) -> {
+            setButtonBindingText();
+            setLabelBindingText();
+
+            Sound buttonSound1 = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+            language.setText("Ngôn Ngữ");
+            LanguageController.switchLanguage(LanguageController.getLanguageLocale(LanguageController.Language.VIETNAMESE));
+            locale = LanguageController.getLocale();
+
+            this.language.setOnMouseClicked((MouseEvent e1) -> {
+                Sound buttonSound2 = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+                language.setText("Language");
+                LanguageController.switchLanguage(LanguageController.getLanguageLocale(LanguageController.Language.ENGLISH));
+                locale = LanguageController.getLocale();
+
+                setButtonActionForLanguage();
+            });
+        });
+    }
+
+    /**************** SETUP BINDING TEXT **************/
+    private void setButtonBindingText() {
+        LanguageController.setUpButtonText(startButton, "menu.start");
+        LanguageController.setUpButtonText(leaderBoard, "menu.leaderBoard");
+        LanguageController.setUpButtonText(instructions, "menu.instruction");
+        LanguageController.setUpButtonText(settings, "menu.setting");
+        LanguageController.setUpButtonText(quit, "menu.quit");
+        LanguageController.setUpButtonText(theme, "setting.Theme");
+        LanguageController.setUpButtonText(backButton3, "setting.Back");
+        LanguageController.setUpButtonText(backButton2, "setting.Back");
+        LanguageController.setUpButtonText(backButton1, "leaderBoard.back");
+        LanguageController.setUpButtonText(backButton2, "setting.BackButton");
+        LanguageController.setUpButtonText(volume, "setting.Volume");
+    }
+
+    /**************** SETUP LABEL TEXT **************/
+    private void setLabelBindingText() {
+        LanguageController.setUpLabelText(welcome, "menu.welcome");
+        LanguageController.setUpLabelText(leaderBoardLabel, "leaderBoard.LeaderBoard");
+        LanguageController.setUpLabelText(instructionLabel, "instruction.InstructionLabel");
+        LanguageController.setUpLabelText(settingLabel, "setting.SettingLabel");
+
+    }
+
+    private void setButtonActionForTheme() {
+        this.theme.setOnMouseClicked((MouseEvent e) -> {
+            Sound buttonSound1 = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
+        });
     }
 
     public void quit(ActionEvent actionEvent) {
         Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
-
+        setButtonForQuitButton();
     }
 
     public void backMainMenuFromInstruction(MouseEvent mouseEvent) {
@@ -175,15 +264,13 @@ public class MainMain implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Timer timer = new Timer();
-        timer.countTime();
-        readyScene.getChildren().add(timer);
+
     }
 
 
     public void goToOnlineScene(ActionEvent actionEvent) {
         Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
-        GameBoardService.mode = true;
+
         chooseModeScene.setVisible(false);
         logInScene.setVisible(true);
     }
@@ -203,30 +290,13 @@ public class MainMain implements Initializable {
     }
 
     public void backToModeScene(ActionEvent actionEvent) {
-        Sound buttonSound = new Sound("src/resources/sound/sound_button_click.mp3");     //Make "button sound" when clicked
-
         logInScene.setVisible(false);
         setNameScene.setVisible(false);
         chooseModeScene.setVisible(true);
     }
 
-    public int getTotal() {
-        return total;
+    public void setButtonForQuitButton() {
+        this.quit.setOnMouseClicked(e -> System.exit(1));
     }
 
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
-    //    start game message
-    public void proccessMessageStart(Message message){
-        this.message.setTotal(message.getTotal()+1);
-        if (this.getTotal() == 4) {
-//          start scene Main Game
-        }
-    }
-
-    public void proccessMessageLogIn(Message message) {
-        //
-    }
 }
