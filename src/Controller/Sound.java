fcf99be8f0709f5f8c8d80a9d69db8e3e4ce1908
jimmy-nhk <s3802040@rmit.edu.vn>
2020-09-23 21:12:59 +0700

    Finish GUI , Congrats guysss

--- src/Controller/Sound.java
+++ src/Controller/Sound.java
@@ -9,7 +9,17 @@
         3. Nguyen Dinh Dang Nguyen - s3759957
         4. Bui Thanh Huy - s3740934
         5. Nguyen Phuoc Nhu Phuc - s3819660
-  Acknowledgement: If you use any resources, acknowledge here. Failure to do so will be considered as plagiarism.
+  Acknowledgement:
+https://www.geeksforgeeks.org/stack-class-in-java/ - Stack Class in Java
+https://www.javatpoint.com/enum-in-java#:~:text=Java%20Enums%20can%20be%20thought,own%20data%20type%20like%20classes. - Java Enums
+https://www.geeksforgeeks.org/collections-shuffle-java-examples/ - Collections.shuffle() in Java with Examples
+https://www.javatpoint.com/java-map - Java Map Interface
+https://www.geeksforgeeks.org/serialization-in-java/ - Serialization and Deserialization in Java with Example
+https://www.baeldung.com/a-guide-to-java-sockets - A Guide to Java Sockets
+https://www.educba.com/javafx-alert/ - Introduction to JavaFX Alert
+https://www.tutorialspoint.com/java/util/java_util_locale.htm - Java.util.Locale Class
+https://stackoverflow.com/questions/22490064/how-to-use-javafx-mediaplayer-correctly - How to use JavaFX MediaPlayer correctly?
+https://www.geeksforgeeks.org/javafx-duration-class/ - JavaFX | Duration Class
 */
 
 package Controller;
